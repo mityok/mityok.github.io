26ee7a35534dcb80af6e97be8c82a41d01705a02
Dimitry <ptdimitry@gmail.com>
2025-07-24 22:54:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 console.log('hello3');
-fetch('https://api.npoint.io/74e502af367c773ad03a').then(t=>t.json()).then(console.log);
+//fetch('https://api.npoint.io/74e502af367c773ad03a').then(t=>t.json()).then(console.log);
+const button2 = document.createElement('button');
+button2.textContent = "Full";
+document.body.append(button2);
+button2.addEventListener('click',()=>{
+  console.log('clk2');
+  document.body.requestFullscreen();
+})
 const button = document.createElement('button');
 button.textContent = "Submit";
 document.body.append(button);
